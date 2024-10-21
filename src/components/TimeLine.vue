4f928d14fe21def3cf191f064ff6ba7dfd3b72d9
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-10-21 07:12:42 +0300

    Удаление дефолтных компонентов. Добавление компонента TimeLine.

---
+++ src/components/TimeLine.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+type Period = 'Today' | 'This Week' | 'This Month';
+
+const periods: Period[] = ['Today', 'This Week', 'This Month'];
+const selectedPeriod = ref<Period>(periods[0]);
+
+function selectPeriod(period: Period) {
+  selectedPeriod.value = period;
+}
+</script>
+
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a
+        v-for="period of periods"
+        :key="period"
+        :class="{ 'is-active': period === selectedPeriod }"
+        @click="selectPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </span>
+  </nav>
+</template>
