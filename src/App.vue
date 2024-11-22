9599eb2336f03625bd86cc2183ae2be171875e35
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-11-22 07:57:14 +0300

    Добавление модального окна

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
-import NavBar from './components/NavBar.vue'
+import NavBar from '@/components/NavBar.vue'
+import { computed } from 'vue'
+import { useModal } from '@/composables/modal'
+
+const modal = useModal()
+
+const modalStyle = computed(() => {
+  return { display: modal.show.value ? 'block' : 'none' }
+})
 </script>
 
 <template>
+  <div class="modal" style="color: white" :style="modalStyle">
+    <div class="modal-background">
+      <div class="modal-content">
+        <div id="modal"></div>
+      </div>
+    </div>
+
+    <button class="modal-close is-large" @click="modal.hideModal"></button>
+  </div>
   <div class="section">
     <div class="container">
       <NavBar />
