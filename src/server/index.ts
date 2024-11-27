d88d663cb5f8ec5bb79348c9db27e16231d9a82d
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-11-27 06:57:43 +0300

    Формирование куки с токеном

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import jsonwebtoken from 'jsonwebtoken'
+import type { Request, Response } from 'express'
 import { today, thisWeek, thisMonth } from '../posts'
 import type { Post } from '../posts'
 import type { NewUser, User } from '../users'
 
 const app = express()
 app.use(cors())
+app.use(cookieParser())
 app.use(bodyParser.json())
 
 const allPosts = [today, thisWeek, thisMonth]
@@ -22,9 +26,22 @@ app.post<{}, {}, Post>('/posts', (req, res) => {
   res.json(allPosts)
 })
 
+const SECRET = 'my-secret'
+const COOKIE = 'vuejs-jwt'
+function authenticate(id: string, req: Request, res: Response) {
+  console.log('request:', req, 'response:', res)
+  const token = jsonwebtoken.sign({ id }, SECRET, {
+    issuer: 'eugen',
+    expiresIn: '30 days',
+  })
+  console.log('token:', token)
+  res.cookie(COOKIE, token, { httpOnly: true })
+}
+
 app.post<{}, {}, NewUser>('/users', (req, res) => {
   const user: User = { ...req.body, id: (Math.random() * 100000).toFixed() }
   allUsers.push(user)
+  authenticate(user.id, req, res)
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { password, ...rest } = user
   res.json(rest)
