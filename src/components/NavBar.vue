fa2a0b86fcf05e7c6ce0682fb678746c4defbd89
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-11-29 08:27:29 +0300

    Добавление редиректа для неавторизованного пользователя

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import { useModal } from '@/composables/modal'
 import { useUsers } from '@/stores/users'
+import { useRouter } from 'vue-router'
 
 const userStore = useUsers()
 const modal = useModal()
+const router = useRouter()
+
+async function logout() {
+  await userStore.logout()
+  await router.push({ path: '/' })
+}
 </script>
 
 <template>
@@ -11,7 +18,7 @@ const modal = useModal()
     <div class="navbar-end">
       <div v-if="userStore.currentUserId" class="buttons">
         <RouterLink to="/posts/new" class="button"> New Post </RouterLink>
-        <button class="button" @click="userStore.logout">Log Out</button>
+        <button class="button" @click="logout">Log Out</button>
       </div>
 
       <div v-else class="buttons">
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUsers } from '@/stores/users'
 import HomeView from '@/views/HomeView.vue'
 import NewPost from '@/views/NewPost.vue'
 
@@ -12,6 +13,15 @@ const router = createRouter({
     {
       path: '/posts/new',
       component: NewPost,
+      beforeEnter: () => {
+        const usersStore = useUsers()
+
+        if (!usersStore.currentUserId) {
+          return {
+            path: '/',
+          }
+        }
+      },
     },
   ],
 })
