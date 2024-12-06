cb15b195a0f6f7da42719f74bfdd9fb6b6714779
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-12-06 06:57:28 +0300

    Монтирование компонента с Vue test utils

--- src/components/FormInput.spec.ts
+++ src/components/FormInput.spec.ts
@@ -1,6 +1,41 @@
 import { mount } from '@vue/test-utils'
-import { describe, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import FormInput from '@/components/FormInput.vue'
 
 describe('FormInput', () => {
-  it('renders some errors', () => {})
+  it('renders some errors', () => {
+    const wrapper = mount(FormInput, {
+      props: {
+        name: 'foo',
+        modelValue: 'bar',
+        status: {
+          valid: false,
+          message: 'error',
+        },
+        type: 'input',
+      },
+    })
+
+    console.log(wrapper.html())
+
+    expect(wrapper.find('.is-danger').exists()).toBe(true)
+  })
+
+  it('renders no errors', () => {
+    const wrapper = mount(FormInput, {
+      props: {
+        name: 'foo',
+        modelValue: 'bar',
+        status: {
+          valid: true,
+          message: 'error',
+        },
+        type: 'input',
+      },
+    })
+
+    console.log(wrapper.html())
+
+    expect(wrapper.find('.is-danger').exists()).toBe(false)
+  })
 })
